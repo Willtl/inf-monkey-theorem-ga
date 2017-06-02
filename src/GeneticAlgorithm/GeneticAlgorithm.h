a9ae4347e0ca72753719d90ebef3d2e06c38b546
willtl <williantessarolunardi@gmail.com>
2017-06-02 16:47:17 +0200

    v01

---
+++ src/GeneticAlgorithm/GeneticAlgorithm.h
@@ -0,0 +1,28 @@
+/*
+ * GeneticAlgorithm.h
+ *
+ *  Created on: 1 de jun de 2017
+ *      Author: willi
+ */
+
+#ifndef GENETICALGORITHM_GENETICALGORITHM_H_
+#define GENETICALGORITHM_GENETICALGORITHM_H_
+
+#include <vector>
+#include "Population.h"
+#include "Individual.h"
+
+using namespace std;
+
+class GeneticAlgorithm {
+public:
+	GeneticAlgorithm(bool, int, int, vector<char>);
+	virtual ~GeneticAlgorithm();
+
+	Individual tournamentSelection(int, Population*);
+	vector<Individual> crossover(Individual, Individual, vector<char>);
+	void mutate(Individual&);
+	void printBestMonkey(int, Individual&);
+};
+
+#endif /* GENETICALGORITHM_GENETICALGORITHM_H_ */
